9037405e73c8d1c798922c25816082a20fae4baa
unknown <m.gibas@programisci17.centrala.kaczmarski.pl>
2014-12-22 12:28:12 +0100

    Updated FakeItEasy and Include fixe

--- src/EntityFramework.Testing.FakeItEasy/Aef.cs
+++ src/EntityFramework.Testing.FakeItEasy/Aef.cs
@@ -11,6 +11,8 @@ namespace EntityFramework.FakeItEasy
         {
             var fakeDbSet = A.Fake<DbSet<T>>(b => b.Implements(typeof(IQueryable<T>)));
 
+            A.CallTo(() => fakeDbSet.Include(A<string>._)).Returns(fakeDbSet);
+
             A.CallTo(() => ((IQueryable<T>)fakeDbSet).Provider).Returns(data.AsQueryable().Provider);
             A.CallTo(() => ((IQueryable<T>)fakeDbSet).Expression).Returns(data.AsQueryable().Expression);
             A.CallTo(() => ((IQueryable<T>)fakeDbSet).ElementType).Returns(data.AsQueryable().ElementType);
@@ -30,6 +32,8 @@ namespace EntityFramework.FakeItEasy
         public static DbSet<T> FakeDbSet<T>(IEnumerable<T> data) where T : class
         {
             var fakeDbSet = A.Fake<DbSet<T>>(b => b.Implements(typeof(IQueryable<T>)));
+
+            A.CallTo(() => fakeDbSet.Include(A<string>._)).Returns(fakeDbSet);
 
             A.CallTo(() => ((IQueryable<T>)fakeDbSet).Provider).Returns(data.AsQueryable().Provider);
             A.CallTo(() => ((IQueryable<T>)fakeDbSet).Expression).Returns(data.AsQueryable().Expression);
