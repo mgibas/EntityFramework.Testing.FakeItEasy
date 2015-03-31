27f4aff4d6d8eeed6cec51a145d97cd73a467476
mgibas <gibasmaciej@gmail.com>
2015-03-31 13:53:16 +0200

    Fix for #1

---
+++ src/EntityFramework.Testing.FakeItEasy.Tests/AefTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EntityFramework.FakeItEasy;
+using NUnit.Framework;
+
+namespace EntityFramework.Testing.FakeItEasy.Tests
+{
+  [TestFixture]
+  public class AefTests
+  {
+    [Test]
+    public void Add_ShouldAddItem()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel> { new TestModel(), new TestModel() });
+      var newModel = new TestModel();
+
+      fakeDbSet.Add(newModel);
+
+      CollectionAssert.Contains(fakeDbSet.ToList(), newModel);
+    }
+
+    [Test]
+    public void Add_ShouldNotThrowWhenIteratingThrough()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel> { new TestModel(), new TestModel() });
+
+      fakeDbSet.Add(new TestModel());
+
+      foreach (var model in fakeDbSet)
+      {
+        Assert.Pass();
+      }
+    }
+
+    [Test]
+    public void AddRange_ShouldAddItems()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel> { new TestModel(), new TestModel() });
+      var newModels = new[] { new TestModel(), new TestModel() };
+
+      fakeDbSet.AddRange(newModels);
+
+      CollectionAssert.IsSubsetOf(newModels, fakeDbSet.ToList());
+    }
+
+    [Test]
+    public void AddRange_ShouldNotThrowWhenIteratingThrough()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel> { new TestModel(), new TestModel() });
+
+      fakeDbSet.AddRange(new[] { new TestModel(), new TestModel() });
+
+      foreach (var model in fakeDbSet)
+      {
+        Assert.Pass();
+      }
+    }
+
+    [Test]
+    public void Remove_ShouldRemoveItem()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel> { new TestModel(), new TestModel() });
+      var toRemove = fakeDbSet.First();
+
+      fakeDbSet.Remove(toRemove);
+
+      CollectionAssert.DoesNotContain(fakeDbSet.ToList(), toRemove);
+    }
+
+    [Test]
+    public void Remove_ShouldNotThrowWhenIteratingThrough()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel> { new TestModel(), new TestModel() });
+
+      fakeDbSet.Remove(fakeDbSet.First());
+
+      foreach (var model in fakeDbSet)
+      {
+        Assert.Pass();
+      }
+    }
+
+    [Test]
+    public void Include_ReturnsItSelf()
+    {
+      var fakeDbSet = Aef.FakeDbSet(new List<TestModel>());
+
+      Assert.AreSame(fakeDbSet, fakeDbSet.Include("asdas"));
+    }
+  }
+
+  public class TestModel
+  {
+  }
+}
--- src/EntityFramework.Testing.FakeItEasy/Aef.cs
+++ src/EntityFramework.Testing.FakeItEasy/Aef.cs
@@ -5,53 +5,57 @@ using System.Linq;
 
 namespace EntityFramework.FakeItEasy
 {
-    public static class Aef
+  public static class Aef
+  {
+    public static DbSet<T> FakeDbSet<T>(IList<T> data) where T : class
     {
-        public static DbSet<T> FakeDbSet<T>(IList<T> data) where T : class
-        {
-            var fakeDbSet = A.Fake<DbSet<T>>(b => b.Implements(typeof(IQueryable<T>)));
+      var fakeDbSet = A.Fake<DbSet<T>>(b => b.Implements(typeof(IQueryable<T>)));
+      A.CallTo(() => fakeDbSet.Include(A<string>._)).Returns(fakeDbSet);
 
-            A.CallTo(() => fakeDbSet.Include(A<string>._)).Returns(fakeDbSet);
+      QueryableSetUp(fakeDbSet, data);
+      CollectionSetUp(fakeDbSet, data);
 
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).Provider).Returns(data.AsQueryable().Provider);
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).Expression).Returns(data.AsQueryable().Expression);
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).ElementType).Returns(data.AsQueryable().ElementType);
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).GetEnumerator()).Returns(data.AsQueryable().GetEnumerator());
-
-            A.CallTo(() => fakeDbSet.Add(A<T>._)).Invokes((T item) => { data.Add(item); });
-            A.CallTo(() => fakeDbSet.AddRange(A<IEnumerable<T>>._)).Invokes((IEnumerable<T> items) =>
-            {
-                foreach (var item in items)
-                    data.Add(item);
-            });
-            A.CallTo(() => fakeDbSet.Remove(A<T>._)).Invokes((T item) => { data.Remove(item); });
-
-            return fakeDbSet;
-        }
-
-        public static DbSet<T> FakeDbSet<T>(IEnumerable<T> data) where T : class
-        {
-            var fakeDbSet = A.Fake<DbSet<T>>(b => b.Implements(typeof(IQueryable<T>)));
-
-            A.CallTo(() => fakeDbSet.Include(A<string>._)).Returns(fakeDbSet);
-
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).Provider).Returns(data.AsQueryable().Provider);
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).Expression).Returns(data.AsQueryable().Expression);
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).ElementType).Returns(data.AsQueryable().ElementType);
-            A.CallTo(() => ((IQueryable<T>)fakeDbSet).GetEnumerator()).Returns(data.AsQueryable().GetEnumerator());
-
-            return fakeDbSet;
-        }
-
-        public static DbSet<T> FakeDbSet<T>(int numberOfFakes) where T : class
-        {
-            var data = A.CollectionOfFake<T>(numberOfFakes);
-            return FakeDbSet(data);
-        }
-
-        public static DbSet<T> FakeDbSet<T>() where T : class
-        {
-            return FakeDbSet(new List<T>());
-        }
+      return fakeDbSet;
     }
+
+    public static DbSet<T> FakeDbSet<T>(IEnumerable<T> data) where T : class
+    {
+      var fakeDbSet = A.Fake<DbSet<T>>(b => b.Implements(typeof(IQueryable<T>)));
+      A.CallTo(() => fakeDbSet.Include(A<string>._)).Returns(fakeDbSet);
+
+      QueryableSetUp(fakeDbSet, data);
+
+      return fakeDbSet;
+    }
+
+    public static DbSet<T> FakeDbSet<T>(int numberOfFakes) where T : class
+    {
+      var data = A.CollectionOfFake<T>(numberOfFakes);
+      return FakeDbSet(data);
+    }
+
+    public static DbSet<T> FakeDbSet<T>() where T : class
+    {
+      return FakeDbSet(new List<T>());
+    }
+
+    private static void QueryableSetUp<T>(IQueryable<T> fakeDbSet, IEnumerable<T> data) where T : class
+    {
+      A.CallTo(() => fakeDbSet.Provider).ReturnsLazily(l => data.AsQueryable().Provider);
+      A.CallTo(() => fakeDbSet.Expression).ReturnsLazily(l => data.AsQueryable().Expression);
+      A.CallTo(() => fakeDbSet.ElementType).ReturnsLazily(l => data.AsQueryable().ElementType);
+      A.CallTo(() => fakeDbSet.GetEnumerator()).ReturnsLazily(l => data.AsQueryable().GetEnumerator());
+    }
+
+    private static void CollectionSetUp<T>(DbSet<T> fakeDbSet, ICollection<T> data) where T : class
+    {
+      A.CallTo(() => fakeDbSet.Add(A<T>._)).Invokes((T item) => data.Add(item));
+      A.CallTo(() => fakeDbSet.AddRange(A<IEnumerable<T>>._)).Invokes((IEnumerable<T> items) =>
+      {
+        foreach (var item in items)
+          data.Add(item);
+      });
+      A.CallTo(() => fakeDbSet.Remove(A<T>._)).Invokes((T item) => data.Remove(item));
+    }
+  }
 }
